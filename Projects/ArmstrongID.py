26e2f7cb177f16d7f09a92ad49a568080057b032
b-junkoo <46694606+b-junkoo@users.noreply.github.com>
2020-06-20 12:18:55 -0400

    Add files via upload

---
+++ Projects/ArmstrongID.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 19 21:07:15 2020
+
+Checks to see if number input is an Armstrong number
+
+@author: Jun
+"""
+
+def armstrong(num):
+    num = str(num)
+    if len(num) == 1:
+        print(num, " is an Armstrong number.")
+    else:
+        numb = 0
+        for n in num:
+            numb += int(n)**len(num)
+        if numb == int(num):
+            print(num, "is an Armstrong number.")
+        else:
+            print(num, "is not Armstrong number.")
+armstrong(1634)
